180252f7b9f1d5c42d5ecf37e3df5dfebbda8130
dzakisecond <dzaki.danen19@gmail.com>
2026-01-30 08:44:55 +0700

    Add files via upload

---
+++ biodata_DanendraDzakiAndita.py
@@ -0,0 +1,18 @@
+str1 = '='
+judul = 'Program Biodata Sederhana dengan Python'
+
+nama = input('Masukkan Nama kamu: ')
+hobi = input('Masukkan Hobi kamu: ')
+alamat = input('Masukkan Alamat kamu: ')
+kota = input('Masukkan Kota kamu: ')
+
+print(str1*30)
+print(judul)
+print(str1*30)
+
+print('Nama   :', nama)
+print('Hobi   :', hobi)
+print('Alamat :', alamat, "\nKota   :", kota)
+
+print(str1*30)
+
